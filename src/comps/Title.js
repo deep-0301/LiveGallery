7b4b82291fe2baa0544862b4c76d6c2087614b2c
Deep Patel <80214651+deep-0301@users.noreply.github.com>
2021-03-25 12:12:51 +0530

    Update Title.js

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>Live Gallery</h1>
+      <h1>Live-Gallery</h1>
       <h2>Upload Pictures</h2>
     </div>
   )
