7da0961569d1c4b4d4e3d3753d4e9320c1d31b0e
deep-0301 <80214651+deep-0301@users.noreply.github.com>
2021-03-25 11:55:24 +0530

    Updates

---
+++ precache-manifest.3ec79b9ff215b1b6c83e677b120a2b9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50fb18af80b90ff7a0b3555f32825d69",
+    "url": "/LiveGallery/index.html"
+  },
+  {
+    "revision": "2c511957f31f3dc07ad1",
+    "url": "/LiveGallery/static/css/main.d655f799.chunk.css"
+  },
+  {
+    "revision": "be2cc0216b9a587dd8ec",
+    "url": "/LiveGallery/static/js/2.c0d4cac7.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/LiveGallery/static/js/2.c0d4cac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c511957f31f3dc07ad1",
+    "url": "/LiveGallery/static/js/main.df9d2eab.chunk.js"
+  },
+  {
+    "revision": "bd56099a2e31888bcc4d",
+    "url": "/LiveGallery/static/js/runtime-main.29dee7f2.js"
+  }
+]);
